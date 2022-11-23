7b48e244c90f9d77f2348b4bc1f76b691d672703
hermask8 <hermask8@gmail.com>
2022-11-22 20:56:19 -0600

    Proyecto

---
+++ index.js
@@ -0,0 +1,62 @@
+import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+// routes
+import AuthRoute from './routes/AuthRoute.js'
+import UserRoute from './routes/UserRoute.js'
+import PostRoute from './routes/PostRoute.js'
+import UploadRoute from './routes/UploadRoute.js'
+import ChatRoute from './routes/ChatRoute.js'
+import MessageRoute from './routes/MessageRoute.js'
+
+const app = express();
+
+// middleware
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(morgan("common"));
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,access-token");
+  next();
+});
+
+// to serve images inside public folder
+app.use(express.static('public')); 
+app.use('/images', express.static('images'));
+
+dotenv.config();
+
+try {
+  // Connect to the MongoDB cluster
+  mongoose.connect(
+    process.env.MONGO_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true, dbName: "ChatDB" },
+    () => {
+      console.log("Connected to MongoDB");
+    }
+  );
+
+} catch (e) {
+  console.log("could not connect");
+}
+
+app.use('/auth', AuthRoute);
+app.use('/user', UserRoute)
+app.use('/posts', PostRoute)
+app.use('/upload', UploadRoute)
+app.use('/chat', ChatRoute)
+app.use('/message', MessageRoute)
+//comentarioo
+
+
+app.listen(8080, () => {
+  console.log("Backend server is running!");
+});
